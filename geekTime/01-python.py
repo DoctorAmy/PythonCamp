348d6d8c443e756a9765c0671a2910d8c31cb47a
MasterChao <631616788@qq.com>
2022-11-21 00:54:36 +0800

    init python camp
    init python camp

---
+++ geekTime/01-python.py
@@ -0,0 +1,19 @@
+# 如何安装开发环境
+
+# 访问官方网站
+# https://www.python.org/
+
+# Downloads->选择对应的系统版本->选择对应的Python版本
+
+# 选择安装包
+
+# 推荐使用稳定版本
+
+# 如何检验安装成功
+# 1. cmd-> python -V
+
+# Window无法找到,需要配置环境变量: 
+# python 执行文件目录: 环境变量 -> 系统 -> Path -> 添加Pyhon目录
+# pip 执行文件目录: 环境变量 -> 系统 -> Path -> 添加Pyhon目录/Scripts/
+
+
--- geekTime/02-ide.py
+++ geekTime/02-ide.py
@@ -1,21 +1,7 @@
-# PythonCamp
- PythonCamp
-
-# 如何安装开发环境
-
-# 访问官方网站
-# https://www.python.org/
-
-# Downloads->选择对应的系统版本->选择对应的Python版本
-
-# 选择安装包
-
-# 推荐使用稳定版本
-
-# 如何检验安装成功
-# 1. cmd-> python -V /python3 -V
-# Window无法找到,需要配置环境变量: 环境变量 -> 系统 -> Path -> 添加Pyhon目录
-
+# Python 集成开发环境
+# Pycharm
+# VSCode
+# jupyterLab
 
 # 1. Pycharm
 # JetBrains开发 , 区分社区版本和专业版本
@@ -36,4 +22,3 @@
 # 命令行安装
 # pip install jupyter lab
 # pip3.11 install jupyterlab
-
